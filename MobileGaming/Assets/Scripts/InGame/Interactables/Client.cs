86b8f3996bef2dd41e12d56903807da31a86c929
Super Hubert <h.ramiandrasoa@rubika-edu.com>
2023-02-28 00:39:47 +0100

    Client Satisfaction (no point system but method gets called))

--- MobileGaming/Assets/Scripts/InGame/Interactables/Client.cs
+++ MobileGaming/Assets/Scripts/InGame/Interactables/Client.cs
@@ -14,15 +14,18 @@ public class Client : MonoBehaviour
     [Header("Satisfaction Settings")]
     public float baseSatisfaction = 45f;
     private float currentSatisfaction = 0;
+    [SerializeField] private float decayPerSecond = 1f;
     [SerializeField] private float increaseOnProduct;
     
     [Header("Product Settings")]
     public ClientData data;
     private ProductData expectedData => data.productDatas[currentDataIndex];
     
-    public bool isAvailable { get; private set; }
     private int currentDataIndex = 0;
     private Product feedbackProduct;
+
+    private Coroutine satisfactionRoutine;
+    private WaitForSeconds satisfactionWait = new WaitForSeconds(0.1f);
     
     public Product ReceiveProduct(Product product)
     {
@@ -36,42 +39,69 @@ public class Client : MonoBehaviour
     private void NextProduct()
     {
         currentDataIndex++;
+        currentSatisfaction = baseSatisfaction;
         
         feedbackText.text = "Yay";
         
-        StartCoroutine(NewProductDelay());
+        StartCoroutine(NewProductDelayRoutine());
         
-        IEnumerator NewProductDelay()
+        IEnumerator NewProductDelayRoutine()
         {
             yield return new WaitForSeconds(0.5f); // TODO - prob mettre l'expected data a null pendant cette periode
 
+            satisfactionRoutine = StartCoroutine(SatisfactionRoutine());
+            
             if (currentDataIndex >= data.productDatas.Count)
             {
+                IncreasePoints();
                 StopClient();
                 yield break;
             }
             feedbackText.text = $"{data.name} : \n {expectedData.Color} and {expectedData.Shape}";  
         }
+
+        IEnumerator SatisfactionRoutine()
+        {
+            while (currentSatisfaction > 0)
+            {
+                yield return satisfactionWait;
+                currentSatisfaction -= 0.1f * decayPerSecond;
+                UpdateFeedbackImage();
+            }
+            
+            StopClient();
+        }
     }
 
     public void StopClient()
     {
-        isAvailable = true;
         OnClientAvailable?.Invoke();
+        
+        StopCoroutine(satisfactionRoutine);
+        satisfactionRoutine = null;
+        currentSatisfaction = 0;
+        UpdateFeedbackImage();
+    }
+
+    private void IncreasePoints()
+    {
+        //TODO - Increase Points
     }
 
     public void SetData(ClientData newData)
     {
-        isAvailable = false;
-
         data = newData;
         currentDataIndex = -1;
-        currentSatisfaction = baseSatisfaction;
         
         NextProduct();
     }
 
     public event Action OnClientAvailable;
+
+    public void UpdateFeedbackImage()
+    {
+        feedbackImage.fillAmount = currentSatisfaction / baseSatisfaction;
+    }
 }
 
 [Serializable]
